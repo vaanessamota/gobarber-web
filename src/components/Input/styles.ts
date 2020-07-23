dae44524cdab6e34596edc4798eba566d846ebdf
Vanessa <vanessaoliveira2706@gmail.com>
2020-07-23 13:12:54 -0300

    unform and input usability

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,22 +1,41 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
 
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
   padding: 16px;
   width: 100%;
+
+  border: 2px solid #232129;
   color: #666360;
-  display:flex;
+
+  display: flex;
   align-items: center;
 
   & + div {
-      margin-top: 8px;
-    }
+    margin-top: 8px;
+  }
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
 
   input {
-    color: #F4EDE8;
+    color: #f4ede8;
     flex: 1;
     background: transparent;
     border: 0;
@@ -24,11 +43,9 @@ export const Container = styled.div`
     &::placeholder {
       color: #666360;
     }
-
   }
 
   svg {
     margin-right: 16px;
   }
-
 `;
