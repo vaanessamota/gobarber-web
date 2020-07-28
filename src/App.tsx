6bbd7d0b67f6d20274801fb32c94bda8cc152dc9
Vanessa <vanessaoliveira2706@gmail.com>
2020-07-28 10:58:43 -0300

    auth context

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
+import { AuthProvider } from './context/AuthContext';
+
+// Colocar o AuthContext Provider por volta dos componentes que vão receber
+// as infos providas pelo contexto
+
+// value é o valor inicial para o contexto.
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
     <GlobalStyle />
   </>
 );
